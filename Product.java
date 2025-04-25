61392e6d486f3db1059ca7065c2326277d10e9f2
AnupParekar <143533195+AnupParekar@users.noreply.github.com>
2025-04-25 18:20:13 +0530

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,5 +1,6 @@
 package com.entity;
 
+
 import jakarta.persistence.*;
 
 @Entity
@@ -9,54 +10,45 @@ public class Product {
     private Long id;
 
     private String name;
+    private double price;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
-    
-    
-
-	public Product() {
-		super();
-	}
-
-
-	public Product(Long id, String name, Category category) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.category = category;
-	}
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 	public Category getCategory() {
 		return category;
 	}
 
-
 	public void setCategory(Category category) {
 		this.category = category;
 	}
 
-    
+    // getters & setters
 }
+
+
