2ea600fc78ff95f3fa4846d261ea80e9983b6ac8
AnupParekar <143533195+AnupParekar@users.noreply.github.com>
2025-04-25 18:20:51 +0530

    Update ProductRepository.java

--- ProductRepository.java
+++ ProductRepository.java
@@ -1,7 +1,8 @@
-package com.repository;
+package com.repo;
 
-import com.nimap.entity.Product;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.entity.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
+public interface ProductRepository extends JpaRepository<Product, Long> {}
+
