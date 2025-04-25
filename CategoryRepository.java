d41802c901d4748bb79ed8e7086100a35eeb616e
AnupParekar <143533195+AnupParekar@users.noreply.github.com>
2025-04-25 18:19:47 +0530

    Update CategoryRepository.java

--- CategoryRepository.java
+++ CategoryRepository.java
@@ -1,7 +1,8 @@
-package com.repository;
+package com.repo;
 
-import com.nimap.entity.Category;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.entity.Category;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
+public interface CategoryRepository extends JpaRepository<Category, Long> {}
+
